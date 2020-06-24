2e30a523ea142b00686b55f90e13de7fc49d035d
Jean Maillard <jean@up-ri.se>
2020-06-24 16:01:44 +0200

    debut

--- src/protovox.h
+++ src/protovox.h
@@ -216,6 +216,7 @@ const char* Protovox::concatenate( const char* arg1, const char* arg2, const cha
   _result += arg4;
   _result += arg5;
   _result += arg6;
+  DPRINT("DEBUG CONCATENATE : ")DPRINTLN(_result);
   return (const char*)_result.c_str();
 }
 
